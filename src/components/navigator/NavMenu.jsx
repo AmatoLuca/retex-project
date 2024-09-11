7c463ddfb36e397e1c4cbe8c27cb385e0275e58b
Amato Luca <amatoroccoluca@gmail.com>
2024-09-11 18:59:16 +0200

    refactor:  update navigator

--- src/components/navigator/NavMenu.jsx
+++ src/components/navigator/NavMenu.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import NavToggleButton from './NavToggleButton';
 import NavSearch from './NavSearch';
 import NavMainMenu from './NavMainMenu';
 import './NavMenu.scss';
 
-const NavMenu = ({ handleMenuClick, isMenuOpened }) => {
+const NavMenu = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const handleMenuClick = useCallback(() => {
+    setIsMenuOpened((prev) => setIsMenuOpened(!prev));
+  }, []);
+
   return (
     <div className="nav-menu">
       <NavToggleButton
--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -1,17 +1,10 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import NavLinks from './NavLinks';
 import NavMenu from './NavMenu';
-import NavMainMenu from './NavMainMenu';
 import Navbar from './Navbar';
 import './Navigator.scss';
 
 const Navigator = () => {
-  const [isMenuOpened, setIsMenuOpened] = useState(false);
-
-  const handleMenuClick = useCallback(() => {
-    setIsMenuOpened((prev) => setIsMenuOpened(!prev));
-  }, []);
-
   return (
     <div className="nav">
       <div className="nav__row">
@@ -19,10 +12,7 @@ const Navigator = () => {
       </div>
 
       <div className="nav__row">
-        <NavMenu
-          handleMenuClick={handleMenuClick}
-          isMenuOpened={isMenuOpened}
-        />
+        <NavMenu />
       </div>
 
       <div className="nav__row">
